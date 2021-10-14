60edfae910844f609eb7c612d768eb1ff4d4152c
Kittipat <first1801@hotmail.com>
2021-10-15 00:13:50 +0700

    before carousel

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import '../styles/globals.css';
 import { StoreProvider } from '../utils/Store';
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 function MyApp({ Component, pageProps }) {
   return (
     <StoreProvider>
--- pages/index.js
+++ pages/index.js
@@ -1,42 +1,29 @@
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
+import { useEffect } from "react";
 import styles from "../styles/Home.module.css";
 import loginstyle from "../styles/loginpage.module.css";
 import Layout from "../components/loginlayout";
+import { Carousel } from 'react-responsive-carousel';
 export default function Home() {
   return (
     <Layout>
-      <div className={loginstyle.legoimage}>LEGO</div>
-      <div className={loginstyle.inputcontainer}>
-        <label>อีเมลล์</label>
-        <input type="text" className={loginstyle.inputwidth100} />
-        <label>รหัสผ่าน</label>
-        <input type="password" className={loginstyle.inputwidth100} />
-        <div className={loginstyle.loginwithcontainer} style={{margin:"10px 0"}}>
-          <Image
-            src="/images/facebookicon.png"
-            alt="facebookicon"
-            width={30}
-            height={30}
-          />
-          <Image
-            src="/images/googleicon.png"
-            alt="googleicon"
-            width={30}
-            height={30}
-          />
-        </div>
-        <div
-          className={loginstyle.flexcolumncenter}
-        >
-          <Link href="/product/1" ><button className={loginstyle.roundbutton} style={{margin:"10px 0"}}>เข้าสู่ระบบ</button></Link>
-          <Link href="/register"><button className={loginstyle.roundbutton} style={{margin:"5px 0"}}>สมัครสมาชิก</button></Link>
-          <Link href="/forgotpassword">
-            <a className={loginstyle.font12} >ลืมรหัสผ่าน</a>
-          </Link>
-        </div>
-      </div>
+       {/* <Carousel>
+                <div>
+                <Image src="/images/carousel-1.jpg"  layout="fill" />
+                    <p className="legend">Legend 1</p>
+                </div>
+                <div>
+                <Image src="/images/carousel-1.jpg"  layout="fill" />
+                    <p className="legend">Legend 2</p>
+                </div>
+                <div>
+                <Image src="/images/carousel-1.jpg"  layout="fill" />
+                    <p className="legend">Legend 3</p>
+                </div>
+            </Carousel> */}
+
     </Layout>
   );
 }
